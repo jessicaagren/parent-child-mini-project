bd55b857795d5bc4c3bfd2006d6dc48ed55fa4ca
jessicaagren <jessicaagren@hotmail.com>
2025-02-28 16:59:00 +0100

    Add new widgets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.scss';
 import AnimalList from './widgets/AnimalList/AnimalList';
-import BookBox2 from './widgets/BookBox copy/BookBox';
+import ASOIAFCharacterCard from './widgets/ASOIAFCharacterCard/ASOIAFCharacterCard';
+// import BookBox2 from './widgets/BookBox copy/BookBox';
 import BookBox from './widgets/BookBox/BookBox';
 import CarList from './widgets/CarList/CarList';
+import ChooseAColorBox from './widgets/ChooseAColor/ChooseAColorBox/ChooseAColorBox';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <BookBox />
       <CarList />
       <AnimalList />
-      <BookBox2/>
+      {/* <BookBox2/> */}
+      <ChooseAColorBox />
+      <ASOIAFCharacterCard />
     </>
   );
 }
