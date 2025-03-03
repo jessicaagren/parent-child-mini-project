81cc1cfcf463680d8af83eacaf212deebf0f9b56
jessicaagren <jessicaagren@hotmail.com>
2025-03-03 11:48:09 +0100

    Add Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import './Button.scss';
+
+type ButtonProps = {
+  title: string;
+  className?: string;
+  clickFunction: () => void;
+};
+
+function Button({ title, className, clickFunction }: ButtonProps) {
+  const handleClick = () => {
+    clickFunction();
+  };
+  return (
+    <>
+      <button id='thisButton' className={className} onClick={handleClick}>
+        {title}
+      </button>
+    </>
+  );
+}
+
+export default Button;
